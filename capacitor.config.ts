4ab2c882c8401e1e952be4e7f7b81ab63a9ea474
SergioAlejandroGuzmanAlvarez <sealguzmanal@ittepci.edu.mx>
2023-09-14 13:51:09 -0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.movinay',
+  appName: 'movinay',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
